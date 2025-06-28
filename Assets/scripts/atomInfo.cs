9a8f34a1962c136f09a8d91e4fd55b81227a8f5a
abdurisaq <ahh8@sfu.ca>
2025-06-28 11:37:15 -0700

    added a wall game object, and a generic atomic script

---
+++ Assets/scripts/atomInfo.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class atomInfo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public string elementString;
+    public string elementState;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

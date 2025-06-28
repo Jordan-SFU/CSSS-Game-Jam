8e8a23a177281f74853707d154f95fc86e049f85
Jordan-SFU <jsm36@sfu.ca>
2025-06-28 14:19:28 -0700

    merge

--- Assets/scripts/atomInfo.cs
+++ Assets/scripts/atomInfo.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class atomInfo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     public string elementString;
     public string elementState;
     public float elementMass;
+    private TextMeshPro tmp;
+
+    void Start()
+    {
+        // Get the TextMeshPro component and set its text to the elementString
+        tmp = GetComponentInChildren<TextMeshPro>();
+        if (tmp != null)
+        {
+            tmp.text = elementString;
+        }
+        else
+        {
+            Debug.LogWarning("TextMeshPro component not found on " + gameObject.name);
+        }
+    }
 }
